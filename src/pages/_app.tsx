4999d7c2b4900386b09238d22594b7e16f7bd662
Mark Misra <markmisra@gmail.com>
2024-09-19 19:40:32 +0300

    Add lint fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
 import type { AppProps } from 'next/app';
+import { GeistSans } from 'geist/font/sans';
 
 import '@/src/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style
+        jsx
+        global
+      >
+        {`
+          * {
+            font-family: ${GeistSans.style.fontFamily};
+            overflow-y: hidden;
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,15 @@
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export default function Home() {
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 sm:p-20`}
     >
       <header>
         <h1 className="text-3xl">Url Shortener</h1>
       </header>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
         <h1>Hello, world</h1>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://github.com/marked29/url-shortener"
