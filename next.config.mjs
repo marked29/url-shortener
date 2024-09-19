3f631784d8848867297d39b532947ea0adacdcc4
Mark Misra <markmisra@gmail.com>
2024-09-19 21:05:22 +0300

    Add BE integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   transpilePackages: ['geist'],
+  async rewrites() {
+    const apiBaseUrl = process.env.API_BASE_URL || 'http://backend:4000';
+
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${apiBaseUrl}/:path*`,
+      },
+      {
+        source: '/:shortUrl',
+        destination: `${apiBaseUrl}/url/:shortUrl`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/services/url-shortener-service/url-shortener-service.ts
+++ src/services/url-shortener-service/url-shortener-service.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { createHttpService } from '../common/http-service-factory';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://backend:4000',
+  baseURL: 'http://localhost:3000',
 });
 
 export const axiosHttpService = createHttpService(axiosInstance);
